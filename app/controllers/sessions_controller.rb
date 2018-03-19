a4429facdc827a320597336903282b25445adcc5
n_sugiyama <n_sugiyama@n-sugiyamanoMacBook-Pro.local>
2018-03-19 19:10:28 +0900

    Fix: Fix view of dashboard layout and some redirects.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to dashboard_index_path
+      redirect_to datafiles_index
     else
       flash.now[:danger] = "ログインに失敗しました"
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to dashboard_index_path
+      redirect_to datafiles_path
     else
       flash.now[:alert] = '登録に失敗しました'
       render :new
